0a7e2279c43a678f24fb1fc47711f55529612770
gracecarter048-blip <gracecarter048@gmail.com>
2026-05-05 00:58:17 -0500

    Working on final

---
+++ Final/Final.cpp
@@ -0,0 +1,88 @@
+// Carter Grace Coding 1 Final
+// Making  the bones of a tamagotchi 
+
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include <vector>
+#include <fstream> 
+
+using namespace std;
+
+void read(string path = "Final.txt") {
+     // create a new var, and try to open file
+    string line;
+    ifstream readFile("Final.txt");      //input file on stream
+    if(readFile.is_open()) {
+        while(getline(readFile, line)) {
+            cout << line << endl;
+        }
+    }
+  
+     //after we are done with file we close it
+    readFile.close();
+
+}
+
+void read(vector<string>& vec, string path = "Final.txt") {
+     string line;
+    ifstream readFile("final.txt");      //input file on stream
+    if(readFile.is_open()) {
+        cout << "Pokedex open.\n";
+        while(getline(readFile, line)) {
+            vec.push_back(line);
+        }
+    }
+    else {
+        cout << "Couldn't open that file.\n";
+    }
+     //after we are done with file we close it
+    readFile.close();
+}
+
+void write(vector<string>& vec, string path = "Final.txt") {
+
+     ofstream writeFile(path);   
+     if(writeFile.is_open()) {
+        for(int i = 0; i < vec.size(); i++) {
+            writeFile << vec[i] << endl;
+        }
+    }
+     else {
+        cout << "couldn't open new writeFile.\n";
+    }
+     
+     writeFile.close();
+}
+
+int main() {
+    cout << "Let's Begin Your Pokemon Training!\n";
+    vector<string> favs;
+    string input;
+
+    string path = "Final.txt";
+    cout << "Would you like to open your pokedex?\n";
+
+    // get players anwser
+     getline(cin, input);
+     if(input != "yes") {
+        path = input;
+    }
+
+    read();     //show current names
+    read(favs); // add the names to the vector
+
+    for(int i = 0; i < final.size(); i++) {
+                 cout << "\t" << final[i] << endl;
+    }
+        
+    cout << "What Pokemon would you like to train to be the best?\n";
+    getline(cin, input);
+            // use the find algorithm
+    Final.push_back(input);
+   
+     
+
+    return 0;
+}
