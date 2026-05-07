5fb82844222b98a9a3764d941ca3c748c5dc73d9
gracecarter048-blip <gracecarter048@gmail.com>
2026-05-07 12:17:17 -0500

    Starting to put the rest of my notes in

--- Final/Final.cpp
+++ Final/Final.cpp
@@ -30,7 +30,7 @@ public:
         cout << "My name is " << name << ".\nI have " << health;
         cout << " health and " << damage << " damage.\n";
     }
-
+    // Create attack code 
     bool attack(starterPokemon& opponent) {
         opponent.health -= damage;
         cout << name << " attacks " << opponent.name << "!\n";
@@ -41,17 +41,7 @@ public:
     }
 };
 
-void read(string path = "Final.txt") {
-     // create a new var, and try to open file
-    string line;
-    ifstream readFile("Final.txt");      //input file on stream
-    if(readFile.is_open()) {
-        while(getline(readFile, line)) {cout << line << endl;}
-    }
-     //after we are done with file we close it
-    readFile.close();
 
-}
 
 void read(vector<string>& vec, string path = "Final.txt") {
     string line;
@@ -64,18 +54,7 @@ void read(vector<string>& vec, string path = "Final.txt") {
     readFile.close();
 }
 
-void write(vector<string>& vec, string path = "Final.txt") {
 
-     ofstream writeFile(path);   
-     if(writeFile.is_open()) {
-        for(int i = 0; i < vec.size(); i++) {
-            writeFile << vec[i] << endl;
-        }
-    }
-     else {cout << "couldn't open new writeFile.\n";}
-     
-     writeFile.close();
-}
 
 int main() {
     cout << "Let's Begin Your Pokemon Training!\n";
@@ -86,70 +65,71 @@ int main() {
     cout << "Would you like to open your pokedex?\n";
     // get players anwser
     getline(cin, userInput);
-    if(userInput != "yes") {
-        path = userInput;
-
-    }
-    read();
+    if(userInput != "yes") {path = userInput;}
     read(Final);
 
     cout << "Would you like to 'add' a pokemon, or 'select' a pokemon for battle?\n";
     
+    getline(cin, userInput);
+    // name a critter
+    if(userInput == "add") {
+        //ask player what they want to add
+        for(int i = 0; i < Final.size(); i++) {
+            cout << "\t" << Final[i] << endl;
+        }
+        cout << "Lets add a new pokemon to collection.\n";
+        //get players feedback
         getline(cin, userInput);
+        //show player the new list
+        Final.push_back(userInput);
 
-        if(userInput == "add") {
-            //ask player what they want to add
-            for(int i = 0; i < Final.size(); i++) {
-                cout << "\t" << Final[i] << endl;
-            }
-            cout << "Lets add a new pokemon to collection.\n";
-            //get players feedback
-            getline(cin, userInput);
-            //show player the new list
-            Final.push_back(userInput);
+        cout << "here is your collection:\n";
+        for(int i = 0; i < Final.size(); i++) {
+            cout << "\t" << Final[i] << endl;
+        }   
+    }
 
-            cout << "here is your collection:\n";
-            for(int i = 0; i < Final.size(); i++) {
-                cout << "\t" << Final[i] << endl;
-            }   
+    if(userInput == "select") {
+        read(Final); // add the names to the vector
+        vector<string>::iterator iter;
+        cout << "here are your Pokemon!\n";
+        for(int i = 0; i < Final.size(); i++) {
+            cout << "   " <<Final[i] <<"\n";
         }
-
-        if(userInput == "select") {
-            read(Final); // add the names to the vector
-            vector<string>::iterator iter;
-            cout << "here are your Pokemon!\n";
-            for(int i = 0; i < Final.size(); i++) {
-                cout << "   " <<Final[i] <<"\n";
-            }
-
-            iter = Final.begin();
-            iter += 1;
-            cout << "What pokemon would you like to select?\n";
-            getline(cin, userInput);
-            // use the find algorithm
-            iter = find(Final.begin(), Final.end(), userInput);
-            if(iter != Final.end()) 
-            cout << " " << *iter << " Has 2 Hunger bars, 2 Attack bars, 4 Health.\n";
-        }
-        do{
+        iter = Final.begin();
+        iter += 1;
+        cout << "What pokemon would you like to select?\n";
+        getline(cin, userInput);
+        // use the find algorithm
+        iter = find(Final.begin(), Final.end(), userInput);
+        // this is if players anwser is found
+        if(iter != Final.end()){cout << "We have found that name!\n";}
+        // this is if players anwser can not be found
+        else{ cout << "we couldn't find that name.\n";}
+        cout << " " << *iter << " Has 2 Hunger bars, 2 Attack bars, 4 Health.\n";
+    }
+    do{
         cout << " Would you like to 'feed' him, 'train' him, or 'battle' with him?\n";
         getline(cin, userInput);
         if(userInput == "feed") {
             cout << "You have feed him, he is now full!\n";
             cout << "he is now ready to train or battle with you!\n";
         } 
-        if(userInput == "train") {
+        else if(userInput == "train") {
             cout << "You both will now train, to be the very best!\n";
             cout << "Would you like to add to your attack or health?\n";
             getline(cin, userInput);
-            if(userInput == "attack") {
-                cout << "Attack has raised to 5 bars.\n";}
+            if(userInput == "attack") {cout << "Attack has raised to 5 bars.\n";}
             if(userInput == "health") {
                 cout << "Health has been raised to 6 bars, he now can take serious evolved pokemon blats.\n";}
             cout << "he is now ready to battle with you!\n";
         } 
+        else if(userInput == "quit") {
+            cout << "thanks for playing.\n";
+            break;
+        }
    
-        if(userInput == "battle") {
+        else if(userInput == "battle") {
             cout << "Let's battle some pokemon!\n";
             cout << "You will battle Charizard, his attack is 4, and health is at 10.\n";
             cout << "Would you like to attack or run away?\n";
@@ -190,12 +170,10 @@ int main() {
                 }
                 while(fighterA.health > 0 && fighterB.health > 0) {
                     // fighterA punches
-                    if(fighterA.attack(fighterB))  {
-                        cout << fighterB.name << " has been defeated!\n";}
+                    if(fighterA.attack(fighterB))  {cout << fighterB.name << " has been defeated!\n";}
                     else {
                         // fighterB punches
-                        if(fighterB.attack(fighterA)) {
-                            cout << fighterA.name << " has been defeated!\n";}
+                        if(fighterB.attack(fighterA)) {cout << fighterA.name << " has been defeated!\n";}
                     }
                 }
                 return 0;
