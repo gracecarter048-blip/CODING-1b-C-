c0222594fd181d36c1ea61b3db3090d1c710c5a9
gracecarter048-blip <gracecarter048@gmail.com>
2026-05-09 21:09:49 -0500

    Finished adding My notes and cleaning up my english

--- Final/Final.cpp
+++ Final/Final.cpp
@@ -23,11 +23,6 @@ public:
         health = rand() % 5 + 5;
         damage = rand() % 6 + 3;
     }
-    // tell the player the stats of the pokemon
-    void hello() {
-        cout << "My name is " << name << ".\nI have " << health;
-        cout << " health and " << damage << " damage.\n";
-    }
     // Create attack code 
     bool attack(starterPokemon& opponent) {
         opponent.health -= damage;
@@ -49,8 +44,15 @@ void read(vector<string>& vec, string path = "Final.txt") {
     else { cout << "Couldn't open that file.\n";}
     readFile.close();
 }
+void read(string path = "Final.txt") {
+     // create a new var, and try to open file
+    string line;
+    ifstream readFile("Final.txt");      //input file on stream
+    if(readFile.is_open()) {while(getline(readFile, line)) {cout << line << endl;} }
+    readFile.close();
+}
 
-int main() {
+int main() {    //the beginning of the pokemon game
     cout << "Let's Begin Your Pokemon Training!\n";
     vector<string> Final;
     string userInput;
@@ -60,8 +62,9 @@ int main() {
     // get players anwser
     getline(cin, userInput);
     if(userInput == "yes") {path = userInput;
-    read(Final);
-    cout << "Would you like to 'add' a pokemon, or 'select' a pokemon for battle?\n";}
+        read();
+        read(Final);
+        cout << "Would you like to 'add' a pokemon, or 'select' a pokemon for battle?\n";}
     
     getline(cin, userInput);
     // name a pokemon into the list
@@ -92,79 +95,75 @@ int main() {
         // this is if players anwser is found
         if(iter != Final.end()){cout << "We have found that name!\n";}
         // this is if players anwser can not be found
-        else{ cout << "We couldn't find that name.\n";}
+        else{ cout << "We couldn't find that name.\n";
+        cout << "Thanks for playing!\n";}
         cout << " " << *iter << " Has 2 Hunger bars, 2 Attack bars, 4 Health.\n";
     }
     do{ //give player the option to feed, train or battle with their pokemon
         cout << " Would you like to 'feed' him, 'train' him, or 'battle' with him?\n";
         getline(cin, userInput);
-        if(userInput == "feed") {   //give player verbal feedback for feeding their pokemon
+        if(userInput == "feed") {   
+            //give player verbal feedback for feeding their pokemon
             cout << "You have feed him, he is now full!\n";
             cout << "He is now ready to train or battle with you!\n";
         } 
-        else if(userInput == "train") {
+        else if(userInput == "train") {     
+            // give player option to train to be the very best
             cout << "You both will now train, to be the very best!\n";
             cout << "Would you like to add to your attack or health?\n";
             getline(cin, userInput);
             if(userInput == "attack") {cout << "Attack has raised to 5 bars.\n";}
             if(userInput == "health") {
                 cout << "Health has been raised to 6 bars, he now can take serious evolved pokemon blats.\n";}
-            cout << "he is now ready to battle with you!\n";
-        } 
-        else if(userInput == "quit") {
-            cout << "thanks for playing.\n";
-            break;
-        }
-   
+            cout << "He is now ready to battle with you!\n";
+        } //give the player an option to quit
+        else if(userInput == "quit") { cout << "thanks for playing.\n";break; }
+            //give player option to battle
         else if(userInput == "battle") {
             cout << "Let's battle some pokemon!\n";
             cout << "You will battle Charizard, his attack is 4, and health is at 10.\n";
-            cout << "Would you like to attack or run away?\n";
-            getline(cin, userInput);
-            if(userInput == "run") {
-                cout << "Better luck next time, Thanks for Playing!\n";
-                break;
-            }
+            // give player two options, including an option to give up
+            cout << "Would you like to 'attack' or 'run' away?\n";
+            getline(cin, userInput);        //take players input
+            if(userInput == "run") {cout << "Better luck next time, Thanks for Playing!\n";
+                break; }
             if(userInput == "attack") {
                 srand(time(0)); 
+                // state the two fighting player options
                 starterPokemon Charizard;
-                Charizard.name = "your Pokemon";
-                Charizard.health = 10;
-                Charizard.damage = 4;
+                Charizard.name = "Charizard";
 
                 starterPokemon yourPokemon;
                 yourPokemon.name = "yourPokemon";
-                yourPokemon.health = 7;
-                yourPokemon.damage = 6;
-
-                Charizard.hello();
-                yourPokemon.hello();
 
                 cout << "Let's start the pokemon battle.\n";
 
-                starterPokemon& fighterA = (rand() % 2 == 0) ? Charizard : yourPokemon;
-                starterPokemon& fighterB = (fighterA.name == yourPokemon.name) ? yourPokemon : Charizard;
-
+                starterPokemon temp, temp2;
+                starterPokemon& fighterA = temp;
+                starterPokemon& fighterB = temp2;
                 if(rand() % 2 == 0) {
                     cout << "Charizard blasts first!\n";
                     fighterA = Charizard;
-                    fighterB = yourPokemon;
+                     fighterB = yourPokemon;
                 }
                 else {
                     cout << "You blast first!\n";
                     fighterA = yourPokemon;
                     fighterB = Charizard;
-                }
+                }   //Give a 1 outcome where you lose in the fight
                 while(fighterA.health > 0 && fighterB.health > 0) {
                     // fighterA punches
-                    if(fighterA.attack(fighterB))  {cout << fighterB.name << " has been defeated!\n";}
-                    else {
+                    if(fighterA.attack(fighterB))  {cout << fighterB.name << " has been defeated!\n";
+                    cout << "Damn, better luck next time, go get him healed.\n";
+                }
+                    else {  //give another outcome where you win the fight
                         // fighterB punches
-                        if(fighterB.attack(fighterA)) {cout << fighterA.name << " has been defeated!\n";}
+                        if(fighterB.attack(fighterA)) {cout << fighterA.name << " has been defeated!\n";
+                        cout << "Congrats you have Won!\n";}
                     }
                 }
                 return 0;
             }   
         }
-    } while(true);
+    } while(true);  // End the came
 }
